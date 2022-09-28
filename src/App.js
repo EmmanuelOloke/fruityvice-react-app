f11b2b6bde82592b223cbb6ecead96cbe533e618
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-28 12:07:26 +0100

    created state for fruits, fetched fruits from fruityvice API with fecthFruit function and used the result to update fruit state, useEffect to render fetchedFruits on page load, passed fruits and setFruits as props to components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import './App.css';
 
 import SearchForm from './components/Search/SearchForm';
 import SearchFilter from './components/SearchFilter/SearchFilter';
+import FruitCard from './components/FruitCard/FruitCard';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [fruits, setFruits] = useState([]);
+
+  const fetchFruits = async () => {
+    try {
+      const response = await fetch('https://fruityvice.com/api/fruit/all');
+      const fruits = await response.json();
+      if (response.status === 200) {
+        setFruits(fruits);
+      }
+    } catch (error) {
+      console.log('Something went wrong while fetching the fruits', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFruits();
+  }, []);
+
   return (
     <React.Fragment>
-      <SearchForm />
+      <SearchForm fruits={fruits} setFruits={setFruits} />
       <SearchFilter />
+      <FruitCard fruits={fruits} />
     </React.Fragment>
   );
 }
