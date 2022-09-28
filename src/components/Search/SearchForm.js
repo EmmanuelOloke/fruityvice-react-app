af67f03f663defd34b04557b6f52462f2720b888
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-28 12:12:23 +0100

    search component JSX design using controlled input state, and making fetch request based on search input value, basice error handling with alert (toastr will be used later)

--- src/components/Search/SearchForm.js
+++ src/components/Search/SearchForm.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdKeyboardReturn, MdSearch } from 'react-icons/md';
 
-const SearchForm = () => {
-  return <div>This will be the search component</div>;
+import './search-form.css';
+
+const SearchForm = ({ fruits, setFruits }) => {
+  const [searchInput, setSearchInput] = useState('');
+  const searchedFruit = [];
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!searchInput) {
+      alert('Please enter a fruit to search for');
+      return;
+    }
+
+    // const match = fruits.filter((fruit) => {
+    //   return fruit.name.includes(searchInput);
+    // });
+
+    // setFruits(match);
+
+    try {
+      const response = await fetch(`https://fruityvice.com/api/fruit/${searchInput}`);
+      searchedFruit.push(await response.json());
+
+      if (response.status !== 200) {
+        alert('The Fruit was not found');
+        return;
+      } else {
+        setFruits(searchedFruit);
+      }
+    } catch (error) {
+      console.log('Something went wrong', error);
+    }
+  };
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <div className="search-container">
+        <MdSearch className="search-icon" />
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search for a fruit..."
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button className="return-btn">
+          <MdKeyboardReturn />
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default SearchForm;
