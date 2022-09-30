588450052aa6768fa23aa03adb841d5a940e7a29
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-30 17:06:55 +0100

    grouped options data for the react select component

---
+++ src/components/SearchFilter/docs/data.js
@@ -0,0 +1,37 @@
+export const nutrientOptions = [
+  { value: 'carbohydrates', label: 'Carbohydrates', color: '#5243AA' },
+  { value: 'protein', label: 'Protein', color: '#5243AA' },
+  { value: 'fat', label: 'Fat', color: '#5243AA' },
+  { value: 'calories', label: 'Calories', color: '#5243AA' },
+  { value: 'sugar', label: 'Sugar', color: '#5243AA' },
+];
+
+export const familyOptions = [
+  { value: 'rosaceae', label: 'Rosaceae', color: '#5243AA' },
+  { value: 'rutaceae', label: 'Rutaceae', color: '#5243AA' },
+  { value: 'anacardiaceae', label: 'Anacardiaceae', color: '#5243AA' },
+  { value: 'cucurbitaceae', label: 'Cucurbitaceae', color: '#5243AA' },
+];
+
+export const genusOptions = [
+  { value: 'citrus', label: 'Citrus', color: '#5243AA' },
+  { value: 'malus', label: 'Malus', color: '#5243AA' },
+  { value: 'prunus', label: 'Prunus', color: '#5243AA' },
+  { value: 'rubus', label: 'Rubus', color: '#5243AA' },
+  { value: 'fragaria', label: 'Fragaria', color: '#5243AA' },
+];
+
+export const groupedOptions = [
+  {
+    label: 'Nutrients',
+    options: nutrientOptions,
+  },
+  {
+    label: 'Family',
+    options: familyOptions,
+  },
+  {
+    label: 'Genus',
+    options: genusOptions,
+  },
+];
