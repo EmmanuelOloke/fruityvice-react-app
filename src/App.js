9e65e7c1b58003fbe7a9dcabfcb43bffbea02d45
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-04 21:11:52 +0100

    removed component re-rendering and passed correct props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import "./App.css";
-import AppNavbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import AppNavbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages";
-import About from "./pages/about";
-import Contact from "./pages/contact";
-import ShowDetail from "./pages/ShowDetail";
-import NotFoundPage from "./pages/notfound";
+import Home from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
+import ShowDetail from './pages/ShowDetail';
+import NotFoundPage from './pages/notfound';
 
-import SearchForm from "./components/Search/SearchForm";
-import SearchFilter from "./components/SearchFilter/SearchFilter";
-import FruitCard from "./components/FruitCard/FruitCard";
-import { useState } from "react";
-import { useEffect } from "react";
+import SearchForm from './components/Search/SearchForm';
+import SearchFilter from './components/SearchFilter/SearchFilter';
+import FruitCard from './components/FruitCard/FruitCard';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   const [fruits, setFruits] = useState([]);
@@ -22,33 +22,28 @@ function App() {
 
   const getFruitDetails = async () => {
     try {
-      const response = await fetch(
-        `https://fruityvice.com/api/fruit/${fruitInDetail}`
-      );
-      console.log("response = ", response);
+      const response = await fetch(`https://fruityvice.com/api/fruit/${fruitInDetail}`);
+      console.log('response = ', response);
       const oneFruit = await response.json();
       if (response.status === 200) {
         setFruitinDetail(oneFruit);
         console.log(`oneFruit = `, oneFruit);
       }
     } catch (error) {
-      console.log(
-        "Something went wrong while fetching the fruit details from the server!",
-        error
-      );
+      console.log('Something went wrong while fetching the fruit details from the server!', error);
     }
   };
 
   const fetchFruits = async () => {
     try {
-      const response = await fetch("https://fruityvice.com/api/fruit/all");
+      const response = await fetch('https://fruityvice.com/api/fruit/all');
       const fruits = await response.json();
       if (response.status === 200) {
         setFruits(fruits);
         setFilteredFruits(fruits);
       }
     } catch (error) {
-      console.log("Something went wrong while fetching the fruits", error);
+      console.log('Something went wrong while fetching the fruits', error);
     }
   };
 
@@ -74,9 +69,7 @@ function App() {
         filteredFruits={filteredFruits}
         setFilteredFruits={setFilteredFruits}
       />
-      <FruitCard fruits={filteredFruits} />
-      <SearchFilter />
-      <FruitCard fruits={fruits} getFruitDetails={getFruitDetails} />
+      <FruitCard fruits={filteredFruits} getFruitDetails={getFruitDetails} />
     </React.Fragment>
   );
 }
