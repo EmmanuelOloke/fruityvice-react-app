bfd6a6eb4f80feb5c2d71ce88851e53f53f7bc79
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-06 13:54:47 +0100

    imported style.css, passed appropriate props to SearchForm and SearchFilter components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import SearchForm from "../components/Search/SearchForm";
-import SearchFilter from "../components/SearchFilter/SearchFilter";
-import FruitCard from "../components/FruitCard/FruitCard";
-import { useState } from "react";
-import { useEffect } from "react";
+import React from 'react';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import SearchForm from '../components/Search/SearchForm';
+import SearchFilter from '../components/SearchFilter/SearchFilter';
+import FruitCard from '../components/FruitCard/FruitCard';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+import './style.css';
 
 const Home = () => {
   const [fruits, setFruits] = useState([]);
@@ -13,33 +15,28 @@ const Home = () => {
 
   const getFruitDetails = async () => {
     try {
-      const response = await fetch(
-        `https://fruityvice.com/api/fruit/${fruitInDetail}`
-      );
-      console.log("response = ", response);
+      const response = await fetch(`https://fruityvice.com/api/fruit/${fruitInDetail}`);
+      console.log('response = ', response);
       const oneFruit = await response.json();
       if (response.status === 200) {
         setFruitinDetail(oneFruit);
         console.log(`oneFruit = `, oneFruit);
       }
     } catch (error) {
-      console.log(
-        "Something went wrong while fetching the fruit details from the server!",
-        error
-      );
+      console.log('Something went wrong while fetching the fruit details from the server!', error);
     }
   };
 
   const fetchFruits = async () => {
     try {
-      const response = await fetch("https://fruityvice.com/api/fruit/all");
+      const response = await fetch('https://fruityvice.com/api/fruit/all');
       const fruits = await response.json();
       if (response.status === 200) {
         setFruits(fruits);
         setFilteredFruits(fruits);
       }
     } catch (error) {
-      console.log("Something went wrong while fetching the fruits", error);
+      console.log('Something went wrong while fetching the fruits', error);
     }
   };
 
@@ -49,16 +46,25 @@ const Home = () => {
 
   return (
     <>
-      <SearchForm fruits={fruits} setFruits={setFruits} />
-      <SearchFilter
-        fruits={fruits}
-        filteredFruits={filteredFruits}
-        setFilteredFruits={setFilteredFruits}
-      />
-      <FruitCard fruits={filteredFruits} getFruitDetails={getFruitDetails} /> 
+      <div className="container">
+        <div className="search-components">
+          <SearchForm
+            fruits={fruits}
+            filteredFruits={filteredFruits}
+            setFilteredFruits={setFilteredFruits}
+          />
+          <SearchFilter
+            fruits={fruits}
+            filteredFruits={filteredFruits}
+            setFilteredFruits={setFilteredFruits}
+          />
+        </div>
+
+        <div className="fruit-cards">
+          <FruitCard fruits={filteredFruits} getFruitDetails={getFruitDetails} />
+        </div>
+      </div>
     </>
-     
-    
   );
 };
 
