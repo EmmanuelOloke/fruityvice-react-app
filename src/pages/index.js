cadba6f383067c257c6c26d0095f32644249683e
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-10 21:31:57 +0100

    formatting the conditional rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import SearchForm from '../components/Search/SearchForm';
 import SearchFilter from '../components/SearchFilter/SearchFilter';
 import FruitCard from '../components/FruitCard/FruitCard';
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
 import { useState } from 'react';
 import { useEffect } from 'react';
+
+import Basket from '../assets/empty-fruit-basket.png';
 
 import './style.css';
 
@@ -46,24 +49,54 @@ const Home = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="search-components">
-          <SearchForm
-            fruits={fruits}
-            filteredFruits={filteredFruits}
-            setFilteredFruits={setFilteredFruits}
-          />
-          <SearchFilter
-            fruits={fruits}
-            filteredFruits={filteredFruits}
-            setFilteredFruits={setFilteredFruits}
-          />
-        </div>
+      {filteredFruits.length > 0 ? (
+        <div className="container">
+          <div className="search-components">
+            <SearchForm
+              fruits={fruits}
+              filteredFruits={filteredFruits}
+              setFilteredFruits={setFilteredFruits}
+            />
+            <SearchFilter
+              fruits={fruits}
+              filteredFruits={filteredFruits}
+              setFilteredFruits={setFilteredFruits}
+            />
+          </div>
 
-        <div className="fruit-cards">
-          <FruitCard fruits={filteredFruits} getFruitDetails={getFruitDetails} />
+          <div className="fruit-cards">
+            <FruitCard fruits={filteredFruits} getFruitDetails={getFruitDetails} />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="container">
+          <div className="search-components">
+            <SearchForm
+              fruits={fruits}
+              filteredFruits={filteredFruits}
+              setFilteredFruits={setFilteredFruits}
+            />
+            <SearchFilter
+              fruits={fruits}
+              filteredFruits={filteredFruits}
+              setFilteredFruits={setFilteredFruits}
+            />
+          </div>
+
+          <div className="empty-basket">
+            <div>No Fruit match what you've searched for</div>
+            <img src={Basket} alt="Empty Fruit Basket" />
+            <button
+              className="empty-basket-btn"
+              onClick={() => {
+                window.location.href = '/';
+              }}
+            >
+              Go To Homepage
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
