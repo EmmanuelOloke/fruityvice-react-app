df876403a6c99844cb838fa86c9866b4bb3c6ad7
Bharadwaj <kunabharadwaj520@gmail.com>
2022-10-14 14:18:47 +0100

    making change in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import { MDBFooter } from "mdb-react-ui-kit";
 export default function Footer() {
   return (
     <MDBFooter
-      className="text-center text-lg-left"
-      style={{ backgroundColor: "#D0F0C0" }}
+      className="text-center text-lg-left "
+      style={{ backgroundColor: "#D0F0C0", marginBottom: 0 }}
     >
       <div
         className="text-center p-3"
