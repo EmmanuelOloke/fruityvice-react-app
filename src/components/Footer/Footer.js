e1a8702cb334db0276a64617cd706e1b3cc2d733
Bharadwaj <kunabharadwaj520@gmail.com>
2022-10-14 14:55:33 +0100

    Sticky Footer solved

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { MDBFooter } from "mdb-react-ui-kit";
+import './Footer.css'
+
 
 export default function Footer() {
   return (
-    <MDBFooter
-      className="text-center text-lg-left "
-      style={{ backgroundColor: "#D0F0C0", marginBottom: 0 }}
-    >
-      <div
-        className="text-center p-3"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-      >
-        &copy; {new Date().getFullYear()} Copyright: <a>Minderacodeacademy</a>
-      </div>
-    </MDBFooter>
+    // <MDBFooter
+    //   className="text-center text-lg-left "
+    //   style={{ backgroundColor: "#D0F0C0", marginBottom: 0 }}
+    // >
+    //   <div
+    //     className="text-center p-3"
+    //     style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+    //   >
+    //     &copy; {new Date().getFullYear()} Copyright: <a>Minderacodeacademy</a>
+    //   </div>
+    // </MDBFooter>
+    <div className="footer">
+      Made by Minderacodeacademy Team
+    </div>
   );
 }
