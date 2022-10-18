f106a16828b04ab782245855cf45d5979b4f6360
Faopaw <66549556+Faopaw@users.noreply.github.com>
2022-10-18 15:31:28 +0100

    toast notifications added

--- src/components/Search/SearchForm.js
+++ src/components/Search/SearchForm.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import { MdKeyboardReturn, MdSearch } from 'react-icons/md';
+import React, { useState } from "react";
+import { MdKeyboardReturn, MdSearch } from "react-icons/md";
 
-import './search-form.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./search-form.css";
 
 const SearchForm = ({ fruits, filteredFruits, setFilteredFruits }) => {
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
+
+  const notFoundErrorToast = () => toast.error('The fruit was not found', {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });
+
+    const enterFruitErrorToast = () => toast.error('Please enter a fruit to search for', {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -13,18 +38,21 @@ const SearchForm = ({ fruits, filteredFruits, setFilteredFruits }) => {
     if (filteredFruits.length === 0) setFilteredFruits(fruits);
 
     if (!searchInput) {
-      alert('Please enter a fruit to search for');
+      // alert("Please enter a fruit to search for");
+      enterFruitErrorToast();
       return;
     }
 
     let allFruitsCopy = [...fruits];
     searchFiltered = allFruitsCopy.filter((searchedFruit) => {
       let fruitsInSmallLetters = searchedFruit.name.toLowerCase();
-      if (fruitsInSmallLetters.includes(searchInput.toLowerCase())) return searchedFruit;
+      if (fruitsInSmallLetters.includes(searchInput.toLowerCase()))
+        return searchedFruit;
     });
 
     if (searchFiltered.length === 0) {
-      alert('The fruit was not found');
+      // alert("The fruit was not found");
+      notFoundErrorToast();
       setFilteredFruits(allFruitsCopy);
     } else {
       setFilteredFruits(searchFiltered);
@@ -45,6 +73,18 @@ const SearchForm = ({ fruits, filteredFruits, setFilteredFruits }) => {
           <MdKeyboardReturn />
         </button>
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </form>
   );
 };
