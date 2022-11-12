7e53c61577a47735ee08abd9093d1884cc9b8f3e
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-11-12 23:56:03 +0100

    changed app title and favicon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,28 +1,21 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { GiFruitBowl } from "react-icons/gi";
-import "./style.css";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { GiFruitBowl } from 'react-icons/gi';
+import './style.css';
 
 const AppNavbar = () => {
-
   return (
     <>
-      <Navbar
-        collapseOnSelect
-        expand="sm"
-        fixed="sticky"
-        className="nav_style"
-        variant="light"
-      >
+      <Navbar collapseOnSelect expand="sm" fixed="sticky" className="nav_style" variant="light">
         <Container>
           <Navbar.Brand href="/" className="brand">
             <GiFruitBowl className="img" /> Fruityvice
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className=".navbar-collapse">
-            <Nav className="ms-auto " >
+            <Nav className="ms-auto ">
               <Nav.Link className="links" href="/">
                 Home
               </Nav.Link>
