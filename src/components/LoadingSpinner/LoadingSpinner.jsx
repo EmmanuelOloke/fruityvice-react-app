ef7ab9ca663611f80b578ab8e8eed68ac2c67033
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-11-12 11:07:56 +0100

    added loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './loading-spinner.css';
+
+const LoadingSpinner = () => {
+  return <div className="lds-dual-ring"></div>;
+};
+
+export default LoadingSpinner;
